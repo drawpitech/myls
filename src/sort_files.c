5c348a5e93db72305a2a28fa3e97112f79a26851
Clément <clement2104.boillot@gmail.com>
2023-11-06 20:08:34 +0100

    Add hidden files support for sort_files

--- src/sort_files.c
+++ src/sort_files.c
@@ -24,10 +24,18 @@ void swap_files(struct dirent *file1, struct dirent *file2)
 }
 
 static
+char *get_ptr(char *filename)
+{
+    while (*filename == '.')
+        filename++;
+    return filename;
+}
+
+static
 bool file_is_upper(struct dirent *file1, struct dirent *file2)
 {
-    char filename1[256];
-    char filename2[256];
+    static char filename1[256];
+    static char filename2[256];
 
     if (file1 == NULL || file2 == NULL)
         return true;
@@ -35,21 +43,20 @@ bool file_is_upper(struct dirent *file1, struct dirent *file2)
     my_strcpy(filename2, file2->d_name);
     my_strlowcase(filename1);
     my_strlowcase(filename2);
-    return (my_strcmp(filename1, filename2) > 0);
-}
-
-static
-void bubble_sort_list(ls_t *ls)
-{
-    for (uint32_t i = 0; i < ls->n_files - 1; i++)
-        if (file_is_upper(ls->files + i, ls->files + i + 1))
-            swap_files(ls->files + i, ls->files + i + 1);
+    return (my_strcmp(get_ptr(filename1), get_ptr(filename2)) > 0);
 }
 
 void sort_files(ls_t *ls)
 {
+    uint32_t size;
+    uint32_t x;
+
     if (ls == NULL || ls->files == NULL)
         return;
-    for (uint32_t i = 0; i < ls->n_files - 1; i++)
-        bubble_sort_list(ls);
+    size = ls->n_files - 1;
+    for (uint32_t i = 0; i < size * size; i++) {
+        x = i % size;
+        if (file_is_upper(ls->files + x, ls->files + x + 1))
+            swap_files(ls->files + x, ls->files + x + 1);
+    }
 }
