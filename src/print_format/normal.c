602b8deb610786e3e0dccbe921f9efb89a9bcdea
Clément <clement2104.boillot@gmail.com>
2023-11-07 17:58:34 +0100

    Remove trailing whitespace from normal output

--- src/print_format/normal.c
+++ src/print_format/normal.c
@@ -15,7 +15,10 @@ void ls_output_normal(ls_t *ls)
 {
     directory_t *dir = &ls->directories;
 
-    for (uint32_t i = 0; i < dir->n_files; i++)
-        my_printf("%s  ", dir->files[i]->d_name);
+    if (dir->n_files == 0)
+        return;
+    my_printf("%s", dir->files[0]->d_name);
+    for (uint32_t i = 1; i < dir->n_files; i++)
+        my_printf("  %s", dir->files[i]->d_name);
     my_printf("\n");
 }
