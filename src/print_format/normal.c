be4c767aac15ebcd9a91128047fe73688a57a62a
Clément <clement2104.boillot@gmail.com>
2023-11-10 19:08:42 +0100

    Add quotes around file name when a space is found in normal mode

--- src/print_format/normal.c
+++ src/print_format/normal.c
@@ -13,10 +13,17 @@
 
 void ls_output_normal(ls_t *ls)
 {
+    struct file_s *file;
+
     if (ls == NULL || ls->dir.files == NULL || ls->dir.n_files == 0)
         return;
     my_printf("%s", ls->dir.files[0].filename);
-    for (uint32_t i = 1; i < ls->dir.n_files; i++)
-        my_printf("  %s", ls->dir.files[i].filename);
+    for (uint32_t i = 1; i < ls->dir.n_files; i++) {
+        file = ls->dir.files + i;
+        if (my_strstr(file->filename, " ") == NULL)
+            my_printf("  %s", file->filename);
+        else
+            my_printf("  '%s'", file->filename);
+    }
     my_printf("\n");
 }
